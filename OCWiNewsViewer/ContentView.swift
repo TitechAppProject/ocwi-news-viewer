c8c91a8a98f42d1c4134dbad6637afb3489b3a17
nanashiki <nanashiki.app@gmail.com>
2020-10-04 14:13:59 +0900

    Initial Commit

---
+++ OCWiNewsViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OCWiNewsViewer
+//
+//  Created by nanashiki on 2020/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OCWiNewsViewer/OCWiNewsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCWiNewsViewerApp.swift
+//  OCWiNewsViewer
+//
+//  Created by nanashiki on 2020/10/04.
+//
+
+import SwiftUI
+
+@main
+struct OCWiNewsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
